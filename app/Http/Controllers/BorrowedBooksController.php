29149a0bba00bc16085e14ef0127740e09c48052
Xenon5443 <mtmisola@up.edu.ph>
2026-03-30 19:30:49 +0800

    feat: Initial commit of basic laravel folder structure

---
+++ app/Http/Controllers/BorrowedBooksController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BorrowedBooks;
+use Illuminate\Http\Request;
+
+class BorrowedBooksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BorrowedBooks $borrowedBooks)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BorrowedBooks $borrowedBooks)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BorrowedBooks $borrowedBooks)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BorrowedBooks $borrowedBooks)
+    {
+        //
+    }
+}
