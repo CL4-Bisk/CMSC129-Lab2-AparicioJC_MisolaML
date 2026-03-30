a6d3724874afa20f7b3f1d8100876290119df097
Xenon5443 <mtmisola@up.edu.ph>
2026-03-30 22:29:00 +0800

    feat: Added model

--- app/Models/BorrowedBooks.php
+++ app/Models/BorrowedBooks.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BorrowedBooks extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'genre',
+        'description',
+        'borrower_name',
+        'borrowed_at',
+        'due_at',
+    ];
+
+    protected $casts = [
+        'borrowed_at' => 'datetime',
+        'due_at' => 'datetime',
+    ];
 }
