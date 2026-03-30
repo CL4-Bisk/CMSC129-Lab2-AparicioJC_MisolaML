05c95bacc83b5ea9dbff0840204b4fccac1f6e3f
Xenon5443 <mtmisola@up.edu.ph>
2026-03-30 22:28:19 +0800

    feat: Added web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BorrowedBooksController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('borrowed-books.index');
 });
+
+Route::resource('borrowed-books', BorrowedBooksController::class);
+
+Route::get('borrowed-books/trashed/all', [BorrowedBooksController::class, 'trashed'])->name('borrowed-books.trashed');
+Route::patch('borrowed-books/{id}/restore', [BorrowedBooksController::class, 'restore'])->name('borrowed-books.restore');
+Route::delete('borrowed-books/{id}/force-delete', [BorrowedBooksController::class, 'forceDelete'])->name('borrowed-books.forceDelete');
